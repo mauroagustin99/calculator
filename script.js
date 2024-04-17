ceb320bec7b72d1f3e922029fd8cef8a9a92311f
Mauro Marcosano <mauromarcosano@gmail.com>
2024-04-16 23:57:24 -0300

    Add first styles Add first functions Add HTML structure

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hello world');
+document.addEventListener('DOMContentLoaded', function () {
+  let firstNum;
+  let secondNum;
+  let operator;
+
+  var botones = document.querySelectorAll('.btn');
+  botones.forEach(function (boton) {
+    boton.addEventListener('click', function () {
+      let valor = parseFloat(this.textContent);
+      console.log('El valor del boton presionado es: ', valor);
+    });
+  });
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function operate(firstNum, secondNum, operator) {}
+
+  function buttonClicked(event) {
+    displayValue += event.target.textContent;
+    display.textContent = displayValue;
+  }
+});
