a336316498b2e6ff7e8044337017380f9d9b7c40
Mauro Marcosano <mauromarcosano@gmail.com>
2024-04-18 11:35:07 -0300

    Add button functions Add screen

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let firstNum;
-  let secondNum;
+  const maxDigits = 10;
+  let firstNum = '';
+  let secondNum = '';
   let operator;
 
-  var botones = document.querySelectorAll('.btn');
-  botones.forEach(function (boton) {
-    boton.addEventListener('click', function () {
-      let valor = parseFloat(this.textContent);
-      console.log('El valor del boton presionado es: ', valor);
+  const screen = document.querySelector('.calculator-screen');
+  var btnNumber = document.querySelectorAll('.btn.number');
+  btnNumber.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      if (firstNum.length < maxDigits) {
+        value = parseFloat(this.textContent);
+
+        firstNum += value;
+        console.log('El valor del boton presionado es: ', firstNum);
+        screen.innerHTML = firstNum;
+      }
     });
   });
+
+  var btnOperator = document.querySelectorAll('.btn.operator');
+  btnOperator.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      operator = this.textContent;
+      console.log('El valor del boton presionado es: ', operator);
+      screen.innerHTML = operator;
+    });
+  });
+
   function add(a, b) {
     return a + b;
   }
@@ -26,7 +43,20 @@ document.addEventListener('DOMContentLoaded', function () {
     return a / b;
   }
 
-  function operate(firstNum, secondNum, operator) {}
+  function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+      case '+':
+        return add(firstNum, secondNum);
+      case '-':
+        return subtract(firstNum, secondNum);
+      case '*':
+        return multiply(firstNum, secondNum);
+      case '/':
+        return divide(firstNum, secondNum);
+      default:
+        return 'Operador no válido';
+    }
+  }
 
   function buttonClicked(event) {
     displayValue += event.target.textContent;
